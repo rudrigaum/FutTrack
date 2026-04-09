ee22768f8c25d4a87c67a1722d4f6f74f5c94948
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2026-04-09 08:22:27 -0300

    Initial Commit

---
+++ FutTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FutTrack
+//
+//  Created by Rodrigo Cerqueira Reis on 09/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
