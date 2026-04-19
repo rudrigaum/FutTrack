d964c5948f2d76036c2c88760607f56937465b1c
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2026-04-19 08:20:43 -0300

    style: fix SwiftLint violations in Xcode boilerplate files

--- FutTrackUITests/FutTrackUITestsLaunchTests.swift
+++ FutTrackUITests/FutTrackUITestsLaunchTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 final class FutTrackUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
@@ -21,11 +21,6 @@ final class FutTrackUITestsLaunchTests: XCTestCase {
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-        // XCUIAutomation Documentation
-        // https://developer.apple.com/documentation/xcuiautomation
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
